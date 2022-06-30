af749858709c2578f7037c2bd2a60bc25a0b5b06
varshini079 <104311746+varshini079@users.noreply.github.com>
2022-06-30 17:32:07 +0530

    #include <stdio.h> int main() { 	int t,l,r,i,count=0; 	scanf("%d ",&t); 	while(t--) 	{ 	    scanf("%d	%d ",&l,&r); 	    count=0; 	    for(i=l;i<=r;i++) 	    { 	        if(i%10==2 || i%10==3 || i%10==9) 	        count++; 	    } 	    printf("%d ",count); 	} 	return 0; }

---
+++ _Counting_Pretty_Numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+	int t,l,r,i,count=0;
+	scanf("%d
+",&t);
+	while(t--)
+	{
+	    scanf("%d	%d
+",&l,&r);
+	    count=0;
+	    for(i=l;i<=r;i++)
+	    {
+	        if(i%10==2 || i%10==3 || i%10==9) 
+	        count++;
+	    }
+	    printf("%d
+",count);
+	}
+	return 0;
+}
+
