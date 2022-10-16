b6618ca87da3971986ebe340f4ca6aca543407ed
varshini079 <104311746+varshini079@users.noreply.github.com>
2022-10-16 12:43:42 +0530

    #include<stdio.h>
    int main(){
        
        int n,i,j,cap_a,initial;
        scanf("%d",&n);
        cap_a =  65;
        initial = cap_a+n-1;
        for(i=initial;i>=cap_a;i--){
            for(j=i;j>=cap_a;j--){
                printf("%c ",i);
            }
            printf("
    ");
            
        }
        
        return 0;
    }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -2,27 +2,17 @@
 
 int main(){
     
-    int n,i,j,k;
-    
+    int n,i,j,cap_a,initial;
     scanf("%d",&n);
-    
-    for(i=1;i<=n;i++){
-        for(k=i;k<=n-1;k++){
-            printf(" ");
-        }
-        for(j=1;j<=n;j++){
-            if (i==1 || i==n){
-                printf("*");
-            }
-            else if (j==1 || j==n){
-                printf("*");
-            }
-            else{
-                printf(" ");
-            }
+    cap_a =  65;
+    initial = cap_a+n-1;
+    for(i=initial;i>=cap_a;i--){
+        for(j=i;j>=cap_a;j--){
+            printf("%c ",i);
         }
         printf("
 ");
+        
     }
     
     return 0;
