2c51df3434268aeb0837b2d9939443441c94c82f
varshini079 <104311746+varshini079@users.noreply.github.com>
2022-10-16 10:58:36 +0530

    #include<stdio.h>
    int main()
    {
        
        char str[1000];
        scanf("%s",str);
        int i;
        for (i=0;str[i]!=NULL;i++)
        {
            if (str[i]=='6')
            {
                str[i]='9';
                break;
            }
        }
        printf("%s",str);
        return 0;
    }

---
+++ Maximum_69_Number.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    
+    char str[1000];
+    scanf("%s",str);
+    int i;
+    for (i=0;str[i]!=NULL;i++)
+    {
+        if (str[i]=='6')
+        {
+            str[i]='9';
+            break;
+        }
+    }
+    printf("%s",str);
+    return 0;
+}
